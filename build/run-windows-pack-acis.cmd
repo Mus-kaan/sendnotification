4eac19611aead0852d9defdaa1ec9c15b15400fc
Wuyi Weng <wuweng@microsoft.com>
2020-10-12 07:56:03 -0700

    Add package signing

--- build/run-windows-pack-acis.cmd
+++ build/run-windows-pack-acis.cmd
@@ -6,4 +6,4 @@ dotnet nuget locals all --list
 set ACISPackageManager=%HOMEPATH%\.nuget\packages\acisextensionsdk\1.0.4.1310\Tools\WapdSMEPackageManager_x64.exe
 
 set _ENV=Test
-%ACISPackageManager% BuildPackage -InputDir:C:\Git\Liftr.GenevaActions\src\Liftr.ACIS\bin\Debug\net462 -OutputDir:C:\Git\Liftr.GenevaActions\src\Liftr.ACIS\bin\Debug\AcisPackages\ -PackageName:Liftr_%_ENV% -SMEConfig:LiftrExtension.%_ENV%.config -SMEAssembly:Microsoft.Liftr.ACIS.dll
+%ACISPackageManager% BuildPackage -InputDir:%~dp0..\src\Liftr.ACIS\bin\Release\net462 -OutputDir:%~dp0..\out\AcisPackages\ -PackageName:Liftr_%_ENV% -SMEConfig:LiftrExtension.%_ENV%.config -SMEAssembly:Microsoft.Liftr.ACIS.dll
