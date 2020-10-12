2e8e93663094d6859727148f8e4d2380d67d1177
Wuyi Weng <wuweng@microsoft.com>
2020-10-12 09:09:04 -0700

    set homepath

--- build/run-windows-pack-acis.cmd
+++ build/run-windows-pack-acis.cmd
@@ -3,6 +3,11 @@
 echo "nuget locations:"
 dotnet nuget locals all --list
 
+IF "%HOMEPATH%"=="" ( 
+    echo "set HOMEPATH in CDPx"
+    set HOMEPATH=C:\Users\ContainerAdministrator
+)
+
 set ACISPackageManager=%HOMEPATH%\.nuget\packages\acisextensionsdk\1.0.4.1310\Tools\WapdSMEPackageManager_x64.exe
 echo "ACISPackageManager: %ACISPackageManager%"
 
