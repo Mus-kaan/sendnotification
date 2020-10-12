70064a4cb062286efc70b58c45717f09b9362148
Wuyi Weng <wuweng@microsoft.com>
2020-10-12 08:48:57 -0700

    print location

--- build/run-windows-pack-acis.cmd
+++ build/run-windows-pack-acis.cmd
@@ -4,6 +4,7 @@ echo "nuget locations:"
 dotnet nuget locals all --list
 
 set ACISPackageManager=%HOMEPATH%\.nuget\packages\acisextensionsdk\1.0.4.1310\Tools\WapdSMEPackageManager_x64.exe
+echo "ACISPackageManager: %ACISPackageManager%"
 
 set _ENV=Test
 %ACISPackageManager% BuildPackage -InputDir:%~dp0..\src\Liftr.ACIS\bin\Release\net462 -OutputDir:%~dp0..\out\AcisPackages\ -PackageName:Liftr_%_ENV% -SMEConfig:LiftrExtension.%_ENV%.config -SMEAssembly:Microsoft.Liftr.ACIS.dll
