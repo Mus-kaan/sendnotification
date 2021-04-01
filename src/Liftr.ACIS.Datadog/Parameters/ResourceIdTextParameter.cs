e240f522af88de201bfda08b6b20b98475a07305
Annalise Pucel <anpucel@microsoft.com>
2021-04-01 19:41:02 +0000

    Merged PR 4231430: Support new whale related acis actions
    Updating Acis actions to include the following whale related actions:
    - Remove relationship entity
    - Get relationship entities for monitor
    - Get relationship entities for resource
    - Get monitors for subscription
    - List all monitors
    - Get marketplace by monitor resource
    
    Relies on PRs in Liftr.DataDog and Liftr.Common
    
    Related work items: #9381186

---
+++ src/Liftr.ACIS.Datadog/Parameters/ResourceIdTextParameter.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.WindowsAzure.Wapd.Acis.Contracts;
+
+namespace Microsoft.Liftr.ACIS
+{
+   public class ResourceIdTextParameter : AcisSMETextParameter
+    {
+        public override string Name => "Datadog Monitored Resource Id";
+
+        public override string HelpText => "Datadog Moniotred Resource Id";
+    }
+}
---
+++ src/Liftr.ACIS.Datadog/Parameters/SubscriptionIdTextParameter.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.WindowsAzure.Wapd.Acis.Contracts;
+
+namespace Microsoft.Liftr.ACIS
+{
+    public class SubscriptionIdTextParameter : AcisSMETextParameter
+    {
+        public override string Name => "Subscription Id";
+
+        public override string HelpText => "Subscription Id";
+    }
+}
