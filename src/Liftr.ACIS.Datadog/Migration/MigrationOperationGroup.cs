17ebd5bf6ffd307ea4aace50a8abaac58a7646a5
Krishna Kanth Majety <kmajety@microsoft.com>
2021-11-22 10:40:24 +0000

    Merged PR 5384156: Add Geneva Actions for LF Migration

---
+++ src/Liftr.ACIS.Datadog/Migration/MigrationOperationGroup.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.WindowsAzure.Wapd.Acis.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Liftr.ACIS.Datadog.Migration
+{
+    public class MigrationOperationGroup : AcisSMEOperationGroup
+    {
+        public const string MigrationOperationGroupName = "Migration";
+
+        public override string Name => MigrationOperationGroupName;
+    }
+}
