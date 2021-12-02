81438b9bc33872b886b28daf54460b4c1c03e5ec
Kanupriya Singh <kansingh@microsoft.com>
2021-12-02 12:19:21 +0000

    Merged PR 5422808: Added Geneva Action- Get Monitor Creation Status By Logz Monitor
    Related work items: #12684954

---
+++ src/Liftr.ACIS.Logz/DB Operations/DBOperationGroup.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.WindowsAzure.Wapd.Acis.Contracts;
+
+namespace Microsoft.Liftr.ACIS
+{
+    /// <summary>
+    /// Each Extension contains one more operation groups.
+    /// This enables extension owners to group (categorize) operations into meaningful groups for ease of management.
+    /// </summary>
+    public class DBOperationGroup : AcisSMEOperationGroup
+    {
+        public const string DBOperationGroupName = "DB";
+
+        public override string Name => DBOperationGroupName;
+    }
+}
