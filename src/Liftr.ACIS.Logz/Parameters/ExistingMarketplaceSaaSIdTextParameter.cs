6aeee51c4e302c5c66e9db338eddd2bf6c81f607
Kanupriya Singh <kansingh@microsoft.com>
2022-06-06 10:49:27 +0000

    Merged PR 6167201: Added geneva action to update marketplace entities
    Related work items: #14640277

---
+++ src/Liftr.ACIS.Logz/Parameters/ExistingMarketplaceSaaSIdTextParameter.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.WindowsAzure.Wapd.Acis.Contracts;
+
+namespace Microsoft.Liftr.ACIS
+{
+    public class ExistingMarketplaceSaaSIdTextParameter : AcisSMETextParameter
+    {
+        public override string Name => "Existing Marketplace SaaS id";
+
+        public override string HelpText => "Existing Marketplace SaaS id";
+    }
+}
---
+++ src/Liftr.ACIS.Logz/Parameters/NewMarketplaceSaaSIdTextParameter.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.WindowsAzure.Wapd.Acis.Contracts;
+
+namespace Microsoft.Liftr.ACIS
+{
+    public class NewMarketplaceSaaSIdTextParameter : AcisSMETextParameter
+    {
+        public override string Name => "New Marketplace SaaS id";
+
+        public override string HelpText => "New Marketplace SaaS id";
+    }
+}
